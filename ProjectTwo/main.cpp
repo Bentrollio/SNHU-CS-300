4cc466fb53d46c695f51391ac24a598c8ff9e2ef
Alex Baires <109702084+bentrollio@users.noreply.github.com>
2023-11-30 23:05:49 -0500

    Tree constructed with inorder + search

--- ProjectTwo/main.cpp
+++ ProjectTwo/main.cpp
@@ -2,11 +2,12 @@
  * PROJECT TWO
  */
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,19 +22,165 @@ struct Course {
     }
 };
 
+// Internal structure holds a tree node.
+struct Node {
+    Course course;
+    Node* left;
+    Node* right;
+
+    // default constructor
+    Node() {
+        left = nullptr;
+        right = nullptr;
+    }
+
+    // Use a course to initialize
+    Node(Course someCourse) : Node() {
+        this->course = someCourse;
+    }
+};
+
+/**
+ * Define a class containing data members and methods to
+ * implement a binary search tree
+ */
+class BinarySearchTree {
+
+private:
+    Node* root;
+
+    void addNode(Node* node, Course course);
+    void inOrder(Node* node);
+
+public:
+    BinarySearchTree();
+    virtual ~BinarySearchTree();
+    void InOrder();
+    void Insert(Course course);
+    Course Search(string courseID);
+};
+
+/**
+ * Default constructor
+ */
+BinarySearchTree::BinarySearchTree() {
+    // Initialize root as null pointer
+    root = nullptr;
+}
+
+/**
+ * Destructor
+ */
+BinarySearchTree::~BinarySearchTree() {
+    // recurse from root deleting every node
+}
+
+/**
+ * Traverse the tree in order
+ */
+void BinarySearchTree::InOrder() {
+    // Calls inOrder function with root as argument
+    this->inOrder(root);
+
+}
+
+/**
+ * Insert Course into tree.
+ */
+void BinarySearchTree::Insert(Course course) {
+    // If the tree is empty, this node is the root.
+    if (root == nullptr) {
+        root = new Node(course);
+    }
+        // Otherwise, call the add node function with the root and new course.
+    else {
+        this->addNode(root, course);
+    }
+}
+
+/**
+ * Search for a course
+ */
+Course BinarySearchTree::Search(string courseID) {
+    // Sets current node equal to root
+    // Starts searching from the root
+    Node* current = root;
+
+    // Keeps looping downwards until bottom reached or marching courseID is found
+    while (current != nullptr) {
+        // Base case: Matching course found
+        if (current->course.courseID == courseID) {
+            return current->course;
+        }
+        // If course is less than current node, then traverse left side of tree
+        if (current->course.courseID > courseID) {
+            current = current->left;
+        }
+            // Otherwise, the value is greater than the current node: traverse right side of tree
+        else {
+            current = current->right;
+        }
+    }
+}
+
+/**
+ * Add a course to some node (recursive)
+ *
+ * @param node Current node in tree
+ * @param course Course to be added
+ */
+void BinarySearchTree::addNode(Node* node, Course course) {
+    // Node is larger than the course so add to left subtree.
+    if (node->course.courseID.compare(course.courseID) > 0) {
+        // Base case: If the left child does not exist, insert new code as left child
+        if (node->left == nullptr) {
+            node->left = new Node(course);
+        }
+            // If there is already a left child, recursively call addNode to find spot for new course
+        else {
+            this->addNode(node->left, course);
+        }
+    }
+        // Add to right subtree
+    else {
+        // Base case: If the right child does not exist, insert new node as right child
+        if (node->right == nullptr) {
+            node->right = new Node(course);
+        }
+            // If it already has a right child, recursively call addNode to find spot for new course
+        else {
+            this->addNode(node->right, course);
+        }
+    }
+}
+
+void BinarySearchTree::inOrder(Node* node) {
+    if (node != nullptr) {
+        inOrder(node->left);
+        cout << node->course.courseID << " - " << node->course.courseName << endl;
+        cout << "Prerequisites: ";
+        for (const auto& preReq : node->course.coursePrerequisites) {
+            cout << preReq << " ";
+        }
+        cout << endl;
+        cout << "--------------------------------------------" << endl;
+        inOrder(node->right);
+    }
+}
+
 /**
  * Displays the course information to the console (std::out)
  *
  * @param - Course struct containing the course info
  */
-void displayCourse(Course course) {
+static void displayCourse(Course course) {
     cout << course.courseID << " - " << course.courseName << endl;
     cout << "Prerequisites: ";
     for (const auto& preReq : course.coursePrerequisites) {
         cout << preReq << " ";
     }
     cout << endl;
-    cout << "----------------" << endl;
+    cout << "------------------------------------" << endl;
 }
 
 /**
@@ -44,7 +191,7 @@ void displayCourse(Course course) {
  * @return - A vector of Course objects.
  */
 // Pun intended.
-vector<Course> conStructCourses(vector<vector<string>> contents) {
+static vector<Course> conStructCourses(vector<vector<string>> contents) {
     vector<Course> courses;
     // Iterate through each row
     for (const auto& row : contents) {
@@ -77,7 +224,7 @@ vector<Course> conStructCourses(vector<vector<string>> contents) {
  * @param - A string
  * @return - A string with the first 3 characters trimmed from it.
  */
-string findBOM(string &word) {
+static string findBOM(string &word) {
     if (word.compare(0, 3, "\xEF\xBB\xBF") == 0) {
         word.erase(0, 3);
     }
@@ -94,7 +241,7 @@ string findBOM(string &word) {
  * @param - A string containing the path to the text or csv file being parsed
  * @return - A vector containing a list of CourseIDs parsed from a file.
  */
-vector <string> loadDefaultCourseList(const string& filePath) {
+static vector <string> loadDefaultCourseList(const string& filePath) {
     vector<string> defaultCourses = {};
     vector<string> temp = {};
 
@@ -126,7 +273,7 @@ vector <string> loadDefaultCourseList(const string& filePath) {
     return defaultCourses;
 }
 
-vector<string> verifyPrerequisites(vector<string> &courseTokens, vector<string> &courseCatalog) {
+static vector<string> verifyPrerequisites(vector<string> &courseTokens, vector<string> &courseCatalog) {
 
     if (courseTokens.size() < 2) { // Verifies that each line has at least 2 elements.
         //cout << "ERROR: Course line incomplete." << endl; //FIXME Error handling
@@ -167,7 +314,7 @@ vector<string> verifyPrerequisites(vector<string> &courseTokens, vector<string> 
  * @param - A string containing the path to the text or csv file being parsed.
  * @return - A vector of vectors that stores each course and its prereqs.
  */
-vector<vector<string>> checkDataIntegrity(const string& filePath) {
+static vector<vector<string>> checkDataIntegrity(const string& filePath) {
     vector<string> courseParameters = loadDefaultCourseList(filePath);
     vector<string> tokens = {};
     vector<vector<string>> fileContents = {};
@@ -207,23 +354,84 @@ vector<vector<string>> checkDataIntegrity(const string& filePath) {
     return fileContents;
 }
 
-void printCourseList(vector<Course> courses) {//FIXME change arguments to tree
-    for (int i = 0; i < courses.size(); ++i) {
+static void loadCoursesIntoTree(vector<Course>& courses, BinarySearchTree* tree) {
+    for (Course& course : courses) {
+        tree->Insert(course);
+    }
+    courses.clear();
+}
+
+static void printCourseList(vector<Course> courses) {//FIXME change arguments to tree
+    for (size_t i = 0; i < courses.size(); ++i) {
         displayCourse(courses.at(i));
     }
 }
 
-void printCourse(const string& filePath) { //FIXME search tree for a course
-    cout << "Print a specific course!" << endl;
+static bool compareCourseID(const Course& firstCourse, const Course& secondCourse) {
+    return firstCourse.courseID < secondCourse.courseID;
 }
 
-void mainMenu(const string& path) {
+static vector<Course> sortVector(vector<Course>& sortee) {
+
+    sort(sortee.begin(), sortee.end(), compareCourseID);
+    printCourseList(sortee);
+    return sortee;
+}
+
+static void printSampleSchedule(BinarySearchTree* tree) {
+    cout << "Here is a sample schedule:" << endl;
+    cout << endl;
+    tree->InOrder();
+}
+
+static void printCourseInformation(BinarySearchTree* tree, string courseNumber) {
+    Course course;
+    course = tree->Search(courseNumber);
+
+    if (!course.courseID.empty()) {
+        displayCourse(course);
+    }
+    else {
+        cout << "Course " << courseNumber << " not found in catalog." << endl;
+    }
+}
+
+/**
+ *
+ * Handles and checks command-line arguments during execution and stores the csv file path into a variable
+ * for the mainMenu() function to utilize when it calls loadBids().
+ *
+ * @param argc - number of command line arguments
+ * @param argv - vector with the actual command line arguments
+ */
+string processCommandLine(int argc, char* argv[]) {
+    // Process command line arguments
+    string filePath;
+    switch (argc) {
+        case 2:
+            filePath = argv[1];
+            break;
+        default:
+            filePath = "/Users/abaires/SNHU-CS-300/ProjectTwo/ABCU_Advising_Program_Input.txt";
+            break;
+    }
+    return filePath;
+}
+
+static void mainMenu(const string& path) {
     vector<vector<string>> courseInfo = checkDataIntegrity(path);
     vector<Course> courses = conStructCourses(courseInfo);
+
+    courseInfo.clear(); // Finished verifying data, cleared memory of courseInfo.
+
+    BinarySearchTree* courseTree;
+    courseTree = new BinarySearchTree();
+
     int userInput = {};
+    string courseNumber = {};
 
     while (userInput != 9) {
-        cout << "*********************************" << endl; // FIXME create main menu
+        cout << "*********************************" << endl;
         cout << "|        COURSE PLANNER         |" << endl;
         cout << "*********************************" << endl;
         cout << "|     1. Load Data Structure    |" << endl;
@@ -241,14 +449,17 @@ void mainMenu(const string& path) {
 
         switch (userInput) {
             case 1:
-
-
+                //sortVector(courses); FIXME adjust sort
+                loadCoursesIntoTree(courses, courseTree);
+                printCourseList(courses);
                 break;
             case 2:
-                printCourseList(courses); //FIX ME: Needs to traverse through tree, not vector.
+                printSampleSchedule(courseTree);
                 break;
             case 3:
-                printCourse("test");
+                cout << "What course do you want to know about?" << endl;
+                cin >> courseNumber;
+                printCourseInformation(courseTree, courseNumber);
                 break;
             case 9:
                 cout << "Thank you for using the course planner!" << endl;
@@ -260,17 +471,13 @@ void mainMenu(const string& path) {
     }
 }
 
+/**
+ * The one and only main() method
+ *
+ * @param arg[1] path to CSV file to load from (optional)
+ * @param arg[2] the bid Id to use when searching the list (optional)
+ */
 int main(int argc, char* argv[]) {
-    string filePath;
-    switch (argc) {
-        case 2:
-            filePath = argv[1];
-            break;
-        default:
-            filePath = "/Users/abaires/SNHU-CS-300/ProjectTwo/ABCU_Advising_Program_Input.txt";
-            break;
-    }
-    mainMenu(filePath); // FIXME adjust mainmenu arguments
-
+    mainMenu(processCommandLine(argc, argv));
     return 0;
 }
